6029dce4c6def416930d8591b4f34da570f44e6e
kylieeXD <khayloaftck@gmail.com>
2026-08-02 11:00:24 +0700

    klee: Drop miauthsecret
    Change-Id: Ie0d6cf2983b7908aeccb50467d57b13b73800ca5
    Signed-off-by: kylieeXD <khayloaftck@gmail.com>

--- device.mk
+++ device.mk
@@ -141,7 +141,6 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/proprietary/touch/lib64/libc++.so:recovery/root/system/lib64/klee-touch/libc++.so \
     $(DEVICE_PATH)/proprietary/touch/lib64/libmisight.so:recovery/root/system/lib64/klee-touch/libmisight.so \
     $(DEVICE_PATH)/proprietary/touch/lib64/libmisight.so:recovery/root/vendor/lib64/libmisight.so \
-    $(DEVICE_PATH)/proprietary/odm/lib64/vendor.xiaomi.hardware.miauthsecretd-V1-ndk.so:recovery/root/vendor/lib64/vendor.xiaomi.hardware.miauthsecretd-V1-ndk.so \
     $(DEVICE_PATH)/proprietary/touch/lib64/vendor.xiaomi.hw.touchfeature-V1-ndk.so:recovery/root/system/lib64/klee-touch/vendor.xiaomi.hw.touchfeature-V1-ndk.so \
     $(DEVICE_PATH)/proprietary/vendor/bin/tee-supplicant:recovery/root/vendor/bin/tee-supplicant \
     $(DEVICE_PATH)/proprietary/vendor/bin/hw/android.hardware.gatekeeper-service.mitee:recovery/root/vendor/bin/hw/android.hardware.gatekeeper-service.mitee \
@@ -172,6 +171,3 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/proprietary/vendor/etc/vintf/manifest/android.hardware.weaver-service.nxp.xml:recovery/root/vendor/odm/etc/vintf/manifest/android.hardware.weaver-service.nxp.xml \
     $(DEVICE_PATH)/proprietary/odm/bin/hw/android.hardware.weaver:recovery/root/vendor/odm/bin/hw/android.hardware.weaver \
     $(DEVICE_PATH)/proprietary/odm/lib64/vendor.xiaomi.hardware.cpace-V1-ndk.so:recovery/root/vendor/odm/lib64/vendor.xiaomi.hardware.cpace-V1-ndk.so \
-    $(DEVICE_PATH)/proprietary/odm/bin/hw/vendor.xiaomi.hardware.authsecretd:recovery/root/vendor/odm/bin/hw/vendor.xiaomi.hardware.authsecretd \
-    $(DEVICE_PATH)/proprietary/odm/etc/vintf/manifest/vendor.xiaomi.hardware.authsecretd.xml:recovery/root/vendor/odm/etc/vintf/manifest/vendor.xiaomi.hardware.authsecretd.xml \
-    $(DEVICE_PATH)/proprietary/vendor/lib64/libc++.so:recovery/root/vendor/odm/lib64/libc++.so
